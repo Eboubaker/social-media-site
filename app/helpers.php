4ed1a9eec2545003bdc8575f1eb8e900b1ccaadb
Eboubakker <eboubakkar@gmail.com>
2021-06-03 17:43:23 +0100

    added helper array_rename_key

--- app/helpers.php
+++ app/helpers.php
@@ -8,3 +8,10 @@ if (!function_exists('mimetoextension'))
         return explode('/', $mime)[1];
     }
 }
+if (!function_exists('array_rename_key')) {
+    function array_rename_key(&$array, $old_key, $new_key)
+    {
+        $array[$new_key] = $array[$old_key];
+        unset($array[$old_key]);
+    }
+}
