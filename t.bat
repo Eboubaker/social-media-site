0abc9313c3d2225cc8719088d60ae669f463b5f2
Eboubakker <eboubakkar@gmail.com>
2021-06-04 22:04:55 +0100

    added console helpers

---
+++ t.bat
@@ -0,0 +1,2 @@
+@echo off
+php artisan tinker tinker.php
---
+++ tinker.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+
